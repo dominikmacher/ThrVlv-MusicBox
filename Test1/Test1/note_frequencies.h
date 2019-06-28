f2c970e78d8b35a28ec6651a9b3677401fb9c9e8
Dominik Macher <dominikmacher@live.at>
2019-06-28 09:50:57 +0200

    improved program, added A-team song

---
+++ Test1/Test1/note_frequencies.h
@@ -0,0 +1,30 @@
+/*
+ * NOTE_FREQUENCIES.h
+ *
+ * Created: 28.06.2019 09:26:51
+ *  Author: Dominik
+ */ 
+
+
+#ifndef NOTE_FREQUENCIES_H_
+#define NOTE_FREQUENCIES_H_
+
+
+#define NOTE_G5 784
+#define NOTE_F5 699
+#define NOTE_E5 659
+#define NOTE_DIS5 622
+#define NOTE_D5 587
+#define NOTE_CIS5 554
+#define NOTE_C5 523
+#define NOTE_H4 493
+#define NOTE_B4 466
+#define NOTE_A4 440
+#define NOTE_GIS4 415
+#define NOTE_G4 392
+#define NOTE_F4 349
+#define NOTE_E4 329
+
+
+
+#endif /* NOTE_FREQUENCIES_H_ */

1eaac81ecd0d91da8b9848d790bc150ef336c09b
Dominik Macher <dominikmacher@live.at>
2019-06-29 13:36:11 +0200

    added 3 new songs

--- Test1/Test1/note_frequencies.h
+++ Test1/Test1/note_frequencies.h
@@ -10,7 +10,10 @@
 #define NOTE_FREQUENCIES_H_
 
 
+#define NOTE_A5 880
+#define NOTE_GIS5 830
 #define NOTE_G5 784
+#define NOTE_FIS5 740
 #define NOTE_F5 699
 #define NOTE_E5 659
 #define NOTE_DIS5 622
@@ -22,9 +25,16 @@
 #define NOTE_A4 440
 #define NOTE_GIS4 415
 #define NOTE_G4 392
+#define NOTE_FIS4 370
 #define NOTE_F4 349
 #define NOTE_E4 329
+#define NOTE_DIS4 311
+#define NOTE_D4 294
+#define NOTE_C4 261
+#define NOTE_H3 247
+#define NOTE_B3 233
 
+#define NOTE_PAUSE 0
 
 
 #endif /* NOTE_FREQUENCIES_H_ */
